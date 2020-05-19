828d94f4754239a3c03a8c6dc8ef841f98cf3b85
Agus <rigoomartinez@gmail.com>
2020-05-19 17:38:00 -0600

    Fix file names

---
+++ dist/SCommand.js
@@ -0,0 +1,88 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.formater = exports.readCommand = void 0;
+function readCommand() {
+    var commandsList = [];
+    return {
+        scmdlist: commandsList,
+        isACommand: function (input, selectedOption) {
+            if (typeof (input) === 'string' || input instanceof String) { // -- for one string / possible subs command
+                commandsList.push(exports.formater(input, selectedOption));
+                return this;
+            }
+            else if (input instanceof Array) { // -- for an array / more than one possible subs command
+                for (var _i = 0, input_1 = input; _i < input_1.length; _i++) {
+                    var element = input_1[_i];
+                    commandsList.push(exports.formater(element, selectedOption));
+                }
+                return this;
+            }
+            return this;
+        }
+    };
+}
+exports.readCommand = readCommand;
+// function formater : Cast a substitution command w the sCommand interface!
+exports.formater = function (input, selectedOption) {
+    //let newCommand : SCommand;
+    var cmdPart = input.split('/');
+    if (cmdPart[0] !== 's')
+        throw new Error('Oh no! Incorrect substitution command!');
+    {
+        var sExist = true;
+        if (cmdPart.length !== 4)
+            throw new Error('Oh no! Incorrect substitution command!');
+        {
+            switch (cmdPart[3]) {
+                case 'g': // global substitution!
+                    return {
+                        s: sExist,
+                        expreg: new RegExp(cmdPart[1], 'g'),
+                        flag: cmdPart[3],
+                        substitution: cmdPart[2],
+                        option: selectedOption
+                    };
+                case 'I': // ignores case sensitive
+                    return {
+                        s: sExist,
+                        expreg: new RegExp(cmdPart[1], 'i'),
+                        flag: cmdPart[3],
+                        substitution: cmdPart[2],
+                        option: selectedOption
+                    };
+                case 'p': // print 
+                    return {
+                        s: sExist,
+                        expreg: new RegExp(cmdPart[1]),
+                        flag: cmdPart[3],
+                        substitution: cmdPart[2],
+                        option: selectedOption
+                    };
+                case '':
+                    return {
+                        s: sExist,
+                        expreg: new RegExp(cmdPart[1]),
+                        flag: cmdPart[3],
+                        substitution: cmdPart[2],
+                        option: selectedOption
+                    };
+                default:
+                    if (!(cmdPart[3].match('^w.*$')))
+                        throw new Error("Oh no! Flag is not valid");
+                    { // write a file!
+                        var wFlag = cmdPart[3].split(' ');
+                        return {
+                            s: sExist,
+                            expreg: new RegExp(cmdPart[1]),
+                            flag: wFlag[0],
+                            substitution: cmdPart[2],
+                            option: selectedOption,
+                            fileToWrite: wFlag[1],
+                            writeContent: ''
+                        };
+                    }
+            } // end of swith( flags )
+        } // end if ( validate of format on substitution command )
+    } // end if ( validate of 's' on substitution command )
+};
+//# sourceMappingURL=SCommand.js.map
